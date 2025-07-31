e5dcce0da6c382740e8b426b2f164996cc25c69c
scott931 <scottmugo@gmail.com>
2025-08-01 00:00:04 +0300

    Working Banner Function

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "731099f39cb50ff1"; }
+/******/ 		__webpack_require__.h = function() { return "fe28fe04438a4238"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
